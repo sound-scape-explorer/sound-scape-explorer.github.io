6fa399b949cf40c4f9589c7163c9d0bba2af1882
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-13 01:47:04 +0100

    Update docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -134,6 +134,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['bash'],
     },
   } satisfies Preset.ThemeConfig,
 };
