be250b7cd26d0182955719d330437ccd580a65cd
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-14 18:17:07 +0100

    Inject version to example directory path

---
+++ src/components/ExampleDirectory/index.tsx
@@ -0,0 +1,7 @@
+import {APP_DIRECTORY} from '@site/constants';
+
+import {CodeInline} from '../CodeInline';
+
+const string = `${APP_DIRECTORY}/examples/campaigns`;
+
+export const ExampleDirectory = () => <CodeInline>{string}</CodeInline>;
