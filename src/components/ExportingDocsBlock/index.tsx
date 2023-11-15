857f957394f22b760d8599d46862c8d46bbb1a68
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-15 15:32:05 +0100

    Add command to export next doc

--- src/components/ExportingDocsBlock/index.tsx
+++ src/components/ExportingDocsBlock/index.tsx
@@ -2,16 +2,27 @@ import CodeBlock from '@theme/CodeBlock';
 import {useMemo} from 'react';
 
 interface Props {
+  next?: boolean;
   version?: string;
 }
 
-export const ExportingDocsBlock = ({version}: Props) => {
+export const ExportingDocsBlock = ({next, version}: Props) => {
   const code = useMemo(() => {
-    const titleSuffix = version ? ` ${version}` : '';
-    const fileSuffix = version ? `-${version}` : '';
+    let endpoint = 'https://sound-scape-explorer.github.io/docs';
+    let titleSuffix = '';
+    let fileSuffix = '';
+
+    if (next) {
+      titleSuffix = ' Next';
+      fileSuffix = '-next';
+      endpoint += '/next';
+    } else {
+      titleSuffix = version ? ` ${version}` : '';
+      fileSuffix = version ? `-${version}` : '';
+    }
 
     return `npx docs-to-pdf \\
---initialDocURLs="https://sound-scape-explorer.github.io/docs" \\
+--initialDocURLs="${endpoint}" \\
 --contentSelector="article" \\
 --paginationSelector="a.pagination-nav__link.pagination-nav__link--next" \\
 --excludeSelectors=".margin-vert--xl a,[class^='tocCollapsible'],.breadcrumbs,.theme-edit-this-page" \\
