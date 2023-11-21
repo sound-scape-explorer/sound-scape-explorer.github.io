e597cc4a74ecca44481c52af6d3c99d965a21ee9
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-21 01:48:55 +0100

    Add PWA

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,7 +69,37 @@ const config: Config = {
     ],
   ],
 
-  plugins: [lunrSearch],
+  plugins: [
+    lunrSearch,
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/docusaurus.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
+          },
+        ],
+      },
+    ],
+  ],
   themeConfig: {
     // Replace with your project's social card
     image: 'img/demo.png',
