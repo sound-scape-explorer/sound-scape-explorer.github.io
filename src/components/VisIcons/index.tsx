894a835d30aa069d62e5127871cf3a4290b052ec
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-27 03:23:02 +0100

    Replace static icons with npm package

---
+++ src/components/VisIcons/index.tsx
@@ -0,0 +1,43 @@
+import {IonIcon} from '@ionic/react';
+import {
+  analyticsOutline,
+  barChartOutline,
+  calendarOutline,
+  cloudUploadOutline,
+  cogOutline,
+  colorPaletteOutline,
+  eyeOutline,
+  gridOutline,
+  headsetOutline,
+  helpOutline,
+  layersOutline,
+  listOutline,
+} from 'ionicons/icons';
+
+import styles from './styles.module.css';
+
+interface Props {
+  icon?: string;
+}
+
+const VisIcon = ({icon = cloudUploadOutline}: Props) => {
+  return (
+    <IonIcon
+      className={styles.icon}
+      icon={icon}
+    />
+  );
+};
+
+export const ImportIcon = () => <VisIcon icon={cloudUploadOutline} />;
+export const SettingsIcon = () => <VisIcon icon={cogOutline} />;
+export const HelpIcon = () => <VisIcon icon={helpOutline} />;
+export const SelectionIcon = () => <VisIcon icon={eyeOutline} />;
+export const ColorsIcon = () => <VisIcon icon={colorPaletteOutline} />;
+export const LabelsIcon = () => <VisIcon icon={layersOutline} />;
+export const TimeIcon = () => <VisIcon icon={calendarOutline} />;
+export const DetailsIcon = () => <VisIcon icon={listOutline} />;
+export const AudioIcon = () => <VisIcon icon={headsetOutline} />;
+export const TrajectoriesIcon = () => <VisIcon icon={analyticsOutline} />;
+export const IndicatorsIcon = () => <VisIcon icon={barChartOutline} />;
+export const DigestedIcon = () => <VisIcon icon={gridOutline} />;
